ba336cc921d5ea60ff3f74b875e68d8a55f27596
domadla <domadlangbayan@gmail.com>
2025-05-12 13:26:02 +0800

    feat (electric page): Add header and footer structure to the electric page

--- page/electric/index.php
+++ page/electric/index.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+    <header>
+        <h1>Sixth String</h1>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+        <p>&copy; 2025 Sixth String. All rights reserved.</p>
+    </footer>
 </body>
 </html>
